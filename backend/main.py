c1a0234729446d840fe494d7c501ddda95e2bcf2
Ankan Pal <itsyourap@gmail.com>
2024-06-29 12:01:57 +0530

    [backend] Set up caching

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,20 @@
-from flask import Flask, request
-from bytewise import wiseshield
+import hashlib
 import json
 
+from flask import Flask, request
+from flask_caching import Cache
+
+from bytewise import wiseshield
+
 app = Flask(__name__)
+
+app.config['CACHE_TYPE'] = 'simple'
+cache = Cache(app)
+
+
+def generate_cache_key(data):
+    key = hashlib.md5(json.dumps(data, sort_keys=True).encode()).hexdigest()
+    return f"wiseshield_cache_{key}"
 
 
 @app.route("/")
@@ -12,11 +24,19 @@ def hello_world():
 
 @app.route("/check", methods=["POST"])
 def check_for_phishing_site():
+    cache_key = generate_cache_key(request.get_json())
+    cached_response = cache.get(cache_key)
+    if cached_response:
+        return cached_response
+
     data = request.get_json()
     if 'content' in data and data['content']:
-        return wiseshield.check_url_content(url=data['url'], content=data['content'])
+        response = wiseshield.check_url_content(url=data['url'], content=data['content'])
     else:
-        return wiseshield.check_url(url=data['url'])
+        response = wiseshield.check_url(url=data['url'])
+
+    cache.set(cache_key, response, timeout=86400)
+    return response
 
 
 @app.route("/train", methods=["POST"])
