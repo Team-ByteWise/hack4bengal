20c992ec4309946825cbb4d912e57a56e425438f
Ankan Pal <itsyourap@gmail.com>
2024-06-29 23:50:35 +0530

    [frontend] Finalize add site feature

--- frontend/src/Component/AddSite/AddSite.jsx
+++ frontend/src/Component/AddSite/AddSite.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 
 function AddSite() {
   const [siteUrl, setSiteUrl] = React.useState("");
@@ -6,18 +7,26 @@ function AddSite() {
   const [success, setSuccess] = React.useState(false);
   const handleSubmit = (e) => {
     e.preventDefault();
-    setSubmitted(true);
-    setSuccess(true);// Reset notification
-    setTimeout(() => {
-      setSubmitted(false);
-    }, 3000);
-    console.log("Submitted URL:", url);
+    axios
+      .post("http://localhost:5000/train", { urls: [siteUrl] })
+      .then((res) => {
+        if (res.data.status === "success") {
+          setSubmitted(true);
+          setSuccess(true);
+        } else {
+          setSubmitted(true);
+          setSuccess(false);
+        }
+        setTimeout(() => {
+          setSubmitted(false);
+        }, 3000);
+      });
   };
   return (
     <div className="py-[60px]  bg-[#B0A5F1] flex justify-around">
       <div className="w-2/5 flex flex-col justify-center">
         <span className="text-[45px] font-semibold  text-[#3d3182] ">
-          Don't find a legitimate site ?
+          Don&apos;t find a legitimate site ?
         </span>
         <span className="text-[30px] text-[#3d3182]">
           Request us to add it here
@@ -39,15 +48,16 @@ function AddSite() {
             Submit
           </button>
         </form>
-        {submitted &&  (success ? (
-          <div className="text-[#3d3182] text-xl font-bold" >
-            URL has been submitted!
-          </div>):(
-             <div className="text-[#823131] text-xl font-bold" >
-             Failed to submit URL!
-           </div>)
-          )
-        }
+        {submitted &&
+          (success ? (
+            <div className="text-[#3d3182] text-xl font-bold">
+              URL has been submitted!
+            </div>
+          ) : (
+            <div className="text-[#823131] text-xl font-bold">
+              Failed to submit URL!
+            </div>
+          ))}
       </div>
     </div>
   );
